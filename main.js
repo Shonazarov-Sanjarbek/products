2372a970628ad520fe1c154444b46348794f84c4
Shonazarov-Sanjarbek <shonazarovsanjarbek71@gmail.com>
2024-07-18 18:05:05 +0500

    products

---
+++ main.js
@@ -0,0 +1,48 @@
+const API__PRODUCTS = "https://dummyjson.com"
+const wrapper = document.querySelector(".wrapper")
+const loading = document.querySelector(".loading")
+
+
+console.log(API__PRODUCTS);
+console.log(wrapper);
+
+async function fetchProducts(api) {
+    try {
+        loading.style.display = "block"
+        let respnonse = await fetch(`${api}/products`)
+        console.log(respnonse);
+        respnonse
+                .json()
+                .then((res)=> createCard(res))
+                .catch((err)=> console.log(err)) 
+                .finally(loading.style.display = "none"
+                )     
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+fetchProducts(API__PRODUCTS)
+
+function createCard(data) {
+    data.products.forEach((product) => {
+        console.log(data);
+        console.log(product);
+        let card = document.createElement("div")
+        card.classList.add("card")
+        console.log(card);
+        card.innerHTML = `
+            <img class="img" src="${product.thumbnail}" alt="">
+            <div class="desc">
+            <h3>${product.title}</h3>
+            <p>${product.description}</p>
+            <span>${product.price}</span>
+            <p>${product.category}</p>
+            </div>
+
+        `
+
+        wrapper.appendChild(card)
+    }
+);
+}
